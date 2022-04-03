9d8e8ae9dd6d3e9678b6b3cb5cf155de1b138f5f
programmermohaimin <you@example.com>
2022-04-03 12:44:55 +0600

    done leap year machin

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
-
-
 function button(){
-    let year = document.getElementById("input").Value;
-    if ((year % 4 ==0) && (year % 400 !== 0)(year % 100 ==0)) {
+    let year = document.getElementById("input").value;
+    if  ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
         alert("This is a leap year")
     }else{
         alert("Not a Leap year")
